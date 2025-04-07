3deff8fe35a64970d563cbb4afb5b4f597c96c87
Muhammad Fau Zan Sabani <mfauzan18@student.ub.ac.id>
2025-04-07 17:38:03 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (App::environment('production')){
+            URL::forceScheme('https');
     }
 }
